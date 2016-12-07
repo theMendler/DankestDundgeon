f0a153e1effbb0f148eae9558fb1a4a0b2492ca4
billytsak <btsakalios@gmail.com>
2016-12-06 20:47:59 -0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -4,10 +4,10 @@ public class Boss extends Monster
 	private String specialAttackDescription;
 	private int specialAttack;
 	public boolean isDead() {
-	if(this.getHP() <= 0) {
-		return true;
-	}
-	else {return false;}
+		if(this.getHP() <= 0) {
+			return true;
+		}
+		else {return false;}
 	}
 	
 	public Boss(String name, int hp, int att, int specAttDam)
